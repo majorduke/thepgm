5845cc45b1d4067d5b327ebe65e8c560c38f5a5f
majorduke <36703284+majorduke@users.noreply.github.com>
2018-02-21 14:39:00 -0500

    Update thepgm.f90

--- thepgm.f90
+++ thepgm.f90
@@ -2,3 +2,13 @@
 !
 ! Description (a.o.t. verb phrase): Interactive Pascal's Triangle.
 !
+
+! Main Loop Input - Enter program data, AND control the program.
+!
+! There is a basic need for three commands, each to be a single character: "decrease by one", "stay the same", and "increase by one".
+! These are the commands for controlling the simulation.
+!
+! There can be, perhaps, less than basic needs for two more commands, which might be added later. Each can be one character.
+! * "help" -- display instructions. We developers can do without this for now, and should not do it until we know what works.
+! * "quit" -- exit the program.  "clean exit" is not required. For now, control-C works fine.
+!
