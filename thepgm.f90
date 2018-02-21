a898f3e10536e0e2294f6236cb62ffbc217a4f56
BullCityTom <BullCityTom@users.noreply.github.com>
2018-02-21 22:22:29 +0000

    Update thepgm.f90
    First... Return a character, and a there is a test program.

--- thepgm.f90
+++ thepgm.f90
@@ -2,6 +2,13 @@
 !
 ! Description (a.o.t. verb phrase): Interactive Pascal's Triangle.
 !
+program thepgm
+! test for mainloopinput
+  character*1 mainloopinput
+  do
+    print *,':'/mainloopinput/':'
+  end do
+contains
 
 ! Main Loop Input - Enter program data, AND control the program.
 !
@@ -12,4 +19,8 @@
 ! * "help" -- display instructions. We developers can do without this for now, and should not do it until we know what works.
 ! * "quit" -- exit the program.  "clean exit" is not required. For now, control-C works fine.
 ! These could be the commands for controlling the simulator program.
-!
+  character*1 function mainloopinput
+    character*1        mainloopinput
+    mainloopinput = '&'
+  end function mainloopinput
+end program thepgm
